fad0417ce66ef78b17815a987e5975a5a9bbef77
dbs-montoya <danmontoya001@gmail.com>
2019-07-06 11:59:10 -0400

    finsihed Inpiration page, stills needs linked up tho

---
+++ src/Pages/Inspiration/Grid/InspirationGrid.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './InspirationGrid.scss';
+
+export default function InspirationGrid() {
+    return (
+        <div className='InspirationGrid'>
+            <div className='InspirationGrid-Wrapper'>
+
+                <div className='InspirationGrid-Row InspirationGrid-Row1'>
+                    <div className='InspirationGrid-Row1-Column1'>
+                        <div className='InspirationGrid-Row1-Column1-Top'></div>
+                        <div className='InspirationGrid-Row1-Column1-Bottom'></div>
+                    </div>
+                    <div className='InspirationGrid-Row1-Column2'></div>
+                </div>
+
+
+
+                <div className='InspirationGrid-Row InspirationGrid-Row2'>
+
+                    <div className='InspirationGrid-Row2-Column1'></div>
+                    <div className='InspirationGrid-Row2-Column2'></div>
+                    <div className='InspirationGrid-Row2-Column3'></div>
+
+                </div>
+
+
+                <div className='InspirationGrid-Row InspirationGrid-Row3'>
+
+                    <div className='InspirationGrid-Row3-Column1'></div>
+                    <div className='InspirationGrid-Row3-Column2'></div>
+
+                </div>
+
+
+                <div className='InspirationGrid-Row InspirationGrid-Row4'>
+
+                    <div className='InspirationGrid-Row4-Column1'></div>
+                    <div className='InspirationGrid-Row4-Column2'></div>
+
+                </div>
+
+
+                <div className='InspirationGrid-Row InspirationGrid-Row5'>
+
+                    <div className='InspirationGrid-Row5-Column1'></div>
+                    <div className='InspirationGrid-Row5-Column2'></div>
+                    <div className='InspirationGrid-Row5-Column3'></div>
+
+
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
--- src/Pages/Inspiration/Inspiration.js
+++ src/Pages/Inspiration/Inspiration.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './Inspiration.scss';
+import InspirationHero from './Hero/InspirationHero';
+import InspirationOpening from './OpeningArticle/InspirationOpening';
+import InspirationHook from './Hook/InspirationHook';
+import InspirationGrid from './Grid/InspirationGrid';
+import InspirationClosing from './Closing Article/InspirationClosing';
 
 export default function Inspiration() {
     return (
         <div className='Inspiration'>
-            <h1>Inspiration</h1>
+            <div className='Inspiration-Wrapper'>
+
+                 <div className='Inspiration-TitleBlock'>
+                    <p className='Inspiration-Title'>Get Inspired</p>
+                </div>
+
+
+                <InspirationHero />
+                <InspirationOpening />
+                <InspirationHook />
+                <InspirationGrid />
+                <InspirationClosing />
+
+
+            </div>
         </div>
     )
 }
