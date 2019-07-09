019840b404040686cae3261e97010fd0bfc550ca
dbs-montoya <danmontoya001@gmail.com>
2019-07-08 22:18:52 -0400

    minor Changes

--- src/Recycled/GoogleMp/Map.js
+++ src/Recycled/GoogleMp/Map.js
@@ -2,6 +2,13 @@ import React from 'react';
 import './Map.scss';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
+
+
+
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
+
+
 export default function Map() {
     return (
         <div className='Map'>
